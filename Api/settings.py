79075c430f9c6510fb4b11950232a0f6ffb6ff67
Gervais Jessy <104218910+jessyMihindou@users.noreply.github.com>
2023-07-22 20:52:19 +0100

    deploy

--- Api/settings.py
+++ Api/settings.py
@@ -114,7 +114,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-ALLOWED_HOSTS = ['Gervaisjessy10.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
